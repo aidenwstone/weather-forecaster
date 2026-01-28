df41aebb74c7b3918718e34c00ebcda8bef0a151
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-01-28 16:11:39 -0500

    Set up Location factory

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,12 +1,16 @@
 FactoryBot.define do
   factory :location do
-    address { "MyString" }
-    ip_address { "MyString" }
-    nickname { "MyString" }
-    latitude { "9.99" }
-    longitude { "9.99" }
-    timezone { "MyString" }
-    name { "MyString" }
-    user { nil }
+    address { "404 Ghost St, Nowhereville USA" }
+    nickname { "Home" }
+    latitude { 40.7128 }
+    longitude { 74.0060 }
+    timezone { "America/New_York" }
+    name { "Nowhereville" }
+    user
+
+    trait :with_ip do
+      address { nil }
+      ip_address { "192.0.2.0" }
+    end
   end
 end
