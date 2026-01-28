8d1131465ecaf8aeff6733fa9423093d89684939
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-01-28 17:26:40 -0500

    Validate that address or ip is provided

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ApplicationRecord
   belongs_to :user
+
+  validate :has_address_or_ip
+
+  def has_address_or_ip
+    unless [ address, ip_address ].any?
+      errors.add(:base, "Must include either 'address' or 'ip_address'")
+    end
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe Location, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
   end
+
+  describe 'validations' do
+    describe '#has_address_or_ip' do
+      context 'when :address is provided' do
+        it 'is valid' do
+          location = build(:location)
+          expect(location).to be_valid
+        end
+      end
+
+      context 'when :ip_address is provided' do
+        it 'is valid' do
+          location = build(:location, :with_ip)
+          expect(location).to be_valid
+        end
+      end
+
+      context 'when neither :address nor :ip_address is provided' do
+        it 'is invalid and returns the correct error' do
+          location = build(:location, address: nil, ip_address: nil)
+          location.valid?
+          expect(location.errors[:base]).to include("Must include either 'address' or 'ip_address'")
+        end
+      end
+    end
+  end
 end
