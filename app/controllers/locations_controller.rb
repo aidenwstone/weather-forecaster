2b82a802501b4eae61d82c72606a059aef995adc
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-01-30 13:37:05 -0500

    Implement Location#index & set up View

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,7 @@ class LocationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @locations = current_user.locations
   end
 
   def new
--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Location management", type: :request do
+RSpec.describe "Locations", type: :request do
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+
+  before do
+    sign_in user1
+  end
+
+  describe "GET /locations" do
+    let!(:user1_loc1) { create(:location, name: "Test Location 1", user: user1) }
+    let!(:user1_loc2) { create(:location, name: "Test Location 2", user: user1) }
+    let!(:user2_loc1) { create(:location, name: "Test Location 3", user: user2) }
+
+    before do
+      get locations_path
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns the current user's locations" do
+      expect(response.body).to include(user1_loc1.name)
+      expect(response.body).to include(user1_loc2.name)
+    end
+
+    it "does not return anyone else's locations" do
+      expect(response.body).not_to include(user2_loc1.name)
+    end
+  end
 end
